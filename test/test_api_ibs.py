7ecbb07730da37a93c2194b89ac0264d347d6b2d
OlzhasKALIEV <130698888+OlzhasKALIEV@users.noreply.github.com>
2024-02-28 00:35:36 +0600

    commit_api_test

--- test/test_api_ibs.py
+++ test/test_api_ibs.py
@@ -3,7 +3,49 @@ import pytest
 from utils.constants.routes import APIRoutes
 
 
-@pytest.mark.parametrize("page", ["2", "5", "10"])
+@pytest.mark.parametrize("page", ["1", "2", "5", "10", "100"])
 def test_get_list_users(user_client, page):
     response = user_client.get(APIRoutes.LIST_USERS + page)
-    assert response["page"] == page
+    assert response["page"] == int(page)
+
+
+@pytest.mark.parametrize("id_user", ["1", "2", "5", "10"])
+def test_get_single_user(user_client, id_user):
+    response = user_client.get(APIRoutes.USERS + id_user)
+    assert response["data"]["id"] == int(id_user)
+
+
+@pytest.mark.parametrize("id_user", ["0", "23"])
+def test_get_single_user_error(user_client, id_user):
+    with pytest.raises(AssertionError, match="404"):
+        response = user_client.get(APIRoutes.USERS + id_user)
+        assert response.status_code == 404
+
+
+@pytest.mark.parametrize("page", ["1", "2", "5", "10", "100"])
+def test_get_list_resource(user_client, page):
+    response = user_client.get(APIRoutes.LIST_RESOURCE + page)
+    assert response["page"] == int(page)
+
+
+@pytest.mark.parametrize("id_unknown", ["2", "5", "10"])
+def test_get_single_resource(user_client, id_unknown):
+    response = user_client.get(APIRoutes.SINGLE_RESOURCE + id_unknown)
+    assert response["data"]["id"] == int(id_unknown)
+
+
+@pytest.mark.parametrize("id_unknown", ["0", "23"])
+def test_get_single_resource_error(user_client, id_unknown):
+    with pytest.raises(AssertionError, match="404"):
+        response = user_client.get(APIRoutes.SINGLE_RESOURCE + id_unknown)
+        assert response.status_code == 404
+
+
+@pytest.mark.parametrize(
+    "name, job", [("morpheus", "leader"), ("neo", "hacker"), ("trinity", "warrior")]
+)
+def test_post_create(user_client, name, job):
+    data = {"name": name, "job": job}
+    response = user_client.post(APIRoutes.USERS, data)
+    assert response["name"] == name
+    assert response["job"] == job
--- utils/constants/routes.py
+++ utils/constants/routes.py
@@ -3,10 +3,11 @@ from enum import Enum
 
 class APIRoutes(str, Enum):
     LIST_USERS = '/api/users?page='
+    LIST_RESOURCE = '/api/unknown?page='
     USERS = '/api/users/'
-    RESOURCE = '/api/unknown/'
-    REGISTER = '/api/register'
-    LOGIN = '/api/login'
+    SINGLE_RESOURCE = '/api/unknown/'
+    REGISTER = '/api/register/'
+    LOGIN = '/api/login/'
     DELAYED_RESPONSE = '/api/users?delay='
 
     def __str__(self) -> str:
