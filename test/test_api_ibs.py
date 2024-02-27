7cf7b05e10134a2eacfcf7d8ec7ff8e9a47895d5
OlzhasKALIEV <130698888+OlzhasKALIEV@users.noreply.github.com>
2024-02-28 02:58:41 +0600

    commit_api_test

--- test/test_api_ibs.py
+++ test/test_api_ibs.py
@@ -18,8 +18,7 @@ def test_get_single_user(user_client, id_user):
 @pytest.mark.parametrize("id_user", ["0", "23"])
 def test_get_single_user_error(user_client, id_user):
     with pytest.raises(AssertionError, match="404"):
-        response = user_client.get(APIRoutes.USERS + id_user)
-        assert response.status_code == 404
+        user_client.get(APIRoutes.USERS + id_user)
 
 
 @pytest.mark.parametrize("page", ["1", "2", "5", "10", "100"])
@@ -42,10 +41,103 @@ def test_get_single_resource_error(user_client, id_unknown):
 
 
 @pytest.mark.parametrize(
-    "name, job", [("morpheus", "leader"), ("neo", "hacker"), ("trinity", "warrior")]
+    "name, job",
+    [
+        (
+            "morpheus",
+            "leader",
+        ),
+        ("neo", "hacker"),
+        ("trinity", "warrior"),
+        ("morpheus", "developer"),
+    ],
 )
 def test_post_create(user_client, name, job):
     data = {"name": name, "job": job}
     response = user_client.post(APIRoutes.USERS, data)
     assert response["name"] == name
     assert response["job"] == job
+
+
+@pytest.mark.parametrize(
+    "name, job, id_user",
+    [
+        ("morpheus", "leader", "2"),
+        ("neo", "hacker", "5"),
+        ("trinity", "warrior", "10"),
+        ("morpheus", "developer", "100"),
+    ],
+)
+def test_put_update(user_client, name, job, id_user):
+    data = {"name": name, "job": job}
+    response = user_client.put(APIRoutes.USERS + id_user, data)
+    assert response["name"] == name
+    assert response["job"] == job
+
+
+@pytest.mark.parametrize(
+    "name, job, id_user",
+    [
+        ("morpheus", "leader", "2"),
+        ("neo", "hacker", "5"),
+        ("trinity", "warrior", "10"),
+        ("morpheus", "developer", "100"),
+    ],
+)
+def test_patch_update(user_client, name, job, id_user):
+    data = {"name": name, "job": job}
+    response = user_client.patch(APIRoutes.USERS + id_user, data)
+    assert response["name"] == name
+    assert response["job"] == job
+
+
+@pytest.mark.parametrize("id_user", ["1", "2", "5", "10"])
+def test_delete_users(user_client, id_user):
+    response = user_client.delete(APIRoutes.USERS + id_user)
+    assert response is None
+
+
+@pytest.mark.parametrize(
+    "email, password",
+    [
+        ("george.bluth@reqres.in", "test_password_one"),
+        ("janet.weaver@reqres.in", "test_password_two"),
+        ("emma.wong@reqres.in", "test_password_three"),
+        ("charles.morris@reqres.in", "test_password_four"),
+    ],
+)
+def test_post_register(user_client, email, password):
+    data = {"email": email, "password": password}
+    response = user_client.post(APIRoutes.REGISTER, data, 200)
+    assert "token" in response
+    assert "id" in response
+
+
+@pytest.mark.parametrize(
+    "email",
+    [
+        "george.bluth@reqres.in",
+        "janet.weaver@reqres.in",
+        "emma.wong@reqres.in",
+        "charles.morris@reqres.in",
+    ],
+)
+def test_post_register_error(user_client, email):
+    data = {"email": "george.bluth@reqres.in"}
+    with pytest.raises(AssertionError, match="400"):
+        user_client.post(APIRoutes.REGISTER, data)
+
+
+@pytest.mark.parametrize(
+    "email, password",
+    [
+        ("george.bluth@reqres.in", "test_password_one"),
+        ("janet.weaver@reqres.in", "test_password_two"),
+        ("emma.wong@reqres.in", "test_password_three"),
+        ("charles.morris@reqres.in", "test_password_four"),
+    ],
+)
+def test_post_login(user_client, email, password):
+    data = {"email": email, "password": password}
+    response = user_client.post(APIRoutes.LOGIN, data, 200)
+    assert "token" in response
