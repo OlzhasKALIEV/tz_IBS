72ecb00ab9c9f371602f49f19574ff1307abcf00
OlzhasKALIEV <130698888+OlzhasKALIEV@users.noreply.github.com>
2024-02-28 19:16:17 +0600

    commit_api_test

--- pags/ibs_page.py
+++ pags/ibs_page.py
@@ -1,4 +1,5 @@
 import json
+import time
 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -8,9 +9,45 @@ from pags.base_page_ibs import BasePage
 
 
 class IbsHomePage(BasePage):
-    LIST_USERS = (By.CSS_SELECTOR, 'li[data-id="users"] a[data-key="try-link"]')
-    SINGLE_USERS = (By.CSS_SELECTOR, 'li[data-id="users-single"] a[data-key="try-link"]')
-    SINGLE_USER_NOT_FOUND = (By.CSS_SELECTOR, 'li[data-id="users-single-not-found"] a[data-key="try-link"]')
+    COMMON_SELECTOR = 'a[data-key="try-link"]'
+
+    LIST_USERS = (By.CSS_SELECTOR, 'li[data-id="users"] ' + COMMON_SELECTOR)
+    SINGLE_USERS = (By.CSS_SELECTOR, 'li[data-id="users-single"] ' + COMMON_SELECTOR)
+    SINGLE_USER_NOT_FOUND = (
+        By.CSS_SELECTOR,
+        'li[data-id="users-single-not-found"] ' + COMMON_SELECTOR,
+    )
+    LIST_RESOURCE = (By.CSS_SELECTOR, 'li[data-id="unknown"] ' + COMMON_SELECTOR)
+    SINGLE_RESOURCE = (
+        By.CSS_SELECTOR,
+        'li[data-id="unknown-single"] ' + COMMON_SELECTOR,
+    )
+    SINGLE_RESOURCE_NOT_FOUND = (
+        By.CSS_SELECTOR,
+        'li[data-id="unknown-single-not-found"] ' + COMMON_SELECTOR,
+    )
+    CREATE = (By.CSS_SELECTOR, 'li[data-id="post"] ' + COMMON_SELECTOR)
+    UPDATE_PUT = (By.CSS_SELECTOR, 'li[data-id="put"] ' + COMMON_SELECTOR)
+    UPDATE_PATCH = (By.CSS_SELECTOR, 'li[data-id="patch"] ' + COMMON_SELECTOR)
+    DELETE = (By.CSS_SELECTOR, 'li[data-id="delete"] ' + COMMON_SELECTOR)
+    REGISTER_SUCCESSFUL = (
+        By.CSS_SELECTOR,
+        'li[data-id="register-successful"] ' + COMMON_SELECTOR,
+    )
+    REGISTER_UNSUCCESSFUL = (
+        By.CSS_SELECTOR,
+        'li[data-id="register-unsuccessful"] ' + COMMON_SELECTOR,
+    )
+    LOGIN_SUCCESSFUL = (
+        By.CSS_SELECTOR,
+        'li[data-id="login-successful"] ' + COMMON_SELECTOR,
+    )
+    LOGIN_UNSUCCESSFUL = (
+        By.CSS_SELECTOR,
+        'li[data-id="login-unsuccessful"] ' + COMMON_SELECTOR,
+    )
+    DELAYED_RESPONSE = (By.CSS_SELECTOR, 'li[data-id="delay"] ' + COMMON_SELECTOR)
+
     OUTPUT_RESPONSE = (By.CSS_SELECTOR, 'pre[data-key="output-response"]')
 
     def click_get_list_users(self):
@@ -36,6 +73,127 @@ class IbsHomePage(BasePage):
     def click_get_single_user_not_found(self):
         element = WebDriverWait(self.driver, 10).until(
             EC.element_to_be_clickable(self.SINGLE_USER_NOT_FOUND)
+        )
+        element.click()
+        response_element = WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located(self.OUTPUT_RESPONSE)
+        )
+        return json.loads(response_element.text)
+
+    def click_get_list_resource(self):
+        element = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable(self.LIST_RESOURCE)
+        )
+        element.click()
+        response_element = WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located(self.OUTPUT_RESPONSE)
+        )
+        return json.loads(response_element.text)
+
+    def click_get_single_resource(self):
+        element = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable(self.SINGLE_RESOURCE)
+        )
+        element.click()
+        response_element = WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located(self.OUTPUT_RESPONSE)
+        )
+        return json.loads(response_element.text)
+
+    def click_get_single_resource_not_found(self):
+        element = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable(self.SINGLE_RESOURCE_NOT_FOUND)
+        )
+        element.click()
+        response_element = WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located(self.OUTPUT_RESPONSE)
+        )
+        return json.loads(response_element.text)
+
+    def click_post_create(self):
+        element = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable(self.CREATE)
+        )
+        element.click()
+        response_element = WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located(self.OUTPUT_RESPONSE)
+        )
+        return json.loads(response_element.text)
+
+    def click_put_update(self):
+        element = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable(self.UPDATE_PUT)
+        )
+        element.click()
+        response_element = WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located(self.OUTPUT_RESPONSE)
+        )
+        return json.loads(response_element.text)
+
+    def click_patch_update(self):
+        element = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable(self.UPDATE_PATCH)
+        )
+        element.click()
+        response_element = WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located(self.OUTPUT_RESPONSE)
+        )
+        return json.loads(response_element.text)
+
+    def click_delete(self):
+        element = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable(self.DELETE)
+        )
+        element.click()
+        response_element = WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located(self.OUTPUT_RESPONSE)
+        )
+        response_text = response_element.text.strip() if response_element.text else None
+        return response_text
+
+    def click_post_register_successful(self):
+        element = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable(self.REGISTER_SUCCESSFUL)
+        )
+        element.click()
+        response_element = WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located(self.OUTPUT_RESPONSE)
+        )
+        return json.loads(response_element.text)
+
+    def click_post_register_unsuccessful(self):
+        element = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable(self.REGISTER_UNSUCCESSFUL)
+        )
+        element.click()
+        response_element = WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located(self.OUTPUT_RESPONSE)
+        )
+        return json.loads(response_element.text)
+
+    def click_post_login_successful(self):
+        element = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable(self.LOGIN_SUCCESSFUL)
+        )
+        element.click()
+        response_element = WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located(self.OUTPUT_RESPONSE)
+        )
+        return json.loads(response_element.text)
+
+    def click_post_login_unsuccessful(self):
+        element = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable(self.LOGIN_UNSUCCESSFUL)
+        )
+        element.click()
+        response_element = WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located(self.OUTPUT_RESPONSE)
+        )
+        return json.loads(response_element.text)
+
+    def click_get_delay(self):
+        element = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable(self.DELAYED_RESPONSE)
         )
         element.click()
         response_element = WebDriverWait(self.driver, 10).until(
--- test/test_page_api.py
+++ test/test_page_api.py
@@ -20,3 +20,90 @@ def test_click_get_single_user_not_found(browser, user_client):
     response_api = user_client.get(APIRoutes.USERS + id_user, 404)
     response_page = browser.click_get_single_user_not_found()
     assert response_page == response_api
+
+
+def test_click_get_list_resource(browser, user_client):
+    response_api = user_client.get(APIRoutes.SINGLE_RESOURCE)
+    response_page = browser.click_get_list_resource()
+    assert response_page == response_api
+
+
+def test_click_get_single_resource(browser, user_client):
+    id_user = "2"
+    response_api = user_client.get(APIRoutes.SINGLE_RESOURCE + id_user)
+    response_page = browser.click_get_single_resource()
+    assert response_page == response_api
+
+
+def test_click_get_single_resource_not_found(browser, user_client):
+    id_user = "23"
+    response_api = user_client.get(APIRoutes.SINGLE_RESOURCE + id_user, 404)
+    response_page = browser.click_get_single_resource_not_found()
+    assert response_page == response_api
+
+
+def test_click_post_create(browser, user_client):
+    data = {"name": "morpheus", "job": "leader"}
+    response_api = user_client.post(APIRoutes.USERS, data)
+    response_page = browser.click_post_create()
+    assert response_page["job"] == response_api["job"]
+    assert response_page["name"] == response_api["name"]
+
+
+def test_click_put_update(browser, user_client):
+    id_user = "2"
+    data = {"name": "morpheus", "job": "zion resident"}
+    response_api = user_client.put(APIRoutes.USERS + id_user, data)
+    response_page = browser.click_put_update()
+    assert response_page["job"] == response_api["job"]
+    assert response_page["name"] == response_api["name"]
+
+
+def test_click_patch_update(browser, user_client):
+    id_user = "2"
+    data = {"name": "morpheus", "job": "zion resident"}
+    response_api = user_client.patch(APIRoutes.USERS + id_user, data)
+    response_page = browser.click_patch_update()
+    assert response_page["job"] == response_api["job"]
+    assert response_page["name"] == response_api["name"]
+
+
+def test_click_delete(browser, user_client):
+    id_user = "2"
+    response_api = user_client.delete(APIRoutes.USERS + id_user)
+    response_page = browser.click_delete()
+    assert response_page == response_api
+
+
+def test_click_post_register_successful(browser, user_client):
+    data = {"email": "eve.holt@reqres.in", "password": "pistol"}
+    response_api = user_client.post(APIRoutes.REGISTER, data, 200)
+    response_page = browser.click_post_register_successful()
+    assert response_page == response_api
+
+
+def test_click_post_register_unsuccessful(browser, user_client):
+    data = {"email": "sydney@fife"}
+    response_api = user_client.post(APIRoutes.REGISTER, data, 400)
+    response_page = browser.click_post_register_unsuccessful()
+    assert response_page == response_api
+
+
+def test_click_post_login_successful(browser, user_client):
+    data = {"email": "eve.holt@reqres.in", "password": "cityslicka"}
+    response_api = user_client.post(APIRoutes.LOGIN, data, 200)
+    response_page = browser.click_post_login_successful()
+    assert response_page == response_api
+
+
+def test_click_post_login_unsuccessful(browser, user_client):
+    data = {"email": "peter@klaven"}
+    response_api = user_client.post(APIRoutes.LOGIN, data, 400)
+    response_page = browser.click_post_login_unsuccessful()
+    assert response_page == response_api
+
+
+def test_click_get_delay(browser, user_client):
+    response_api = user_client.get(APIRoutes.DELAYED_RESPONSE)
+    response_page = browser.click_get_delay()
+    assert response_page == response_api
